eceb9a7b75d1f599003445abee43a1a1342854b6
appwebtech <josembi@gmail.com>
2016-05-14 09:39:00 +0200

    Add Completed_at ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,13 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
+    redirect_to root_path 
+  end
+
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
     redirect_to root_path
-    
   end
 
 
